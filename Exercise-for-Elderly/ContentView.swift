9bea80c0a5bcdac16ff82872ae26e3773de75d10
embunazzahra <dhauembunazzahra@gmail.com>
2024-08-12 09:51:47 +0700

    Initial Commit

---
+++ Exercise-for-Elderly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Exercise-for-Elderly
+//
+//  Created by Dhau Embun Azzahra on 12/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Exercise-for-Elderly/Exercise_for_ElderlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise_for_ElderlyApp.swift
+//  Exercise-for-Elderly
+//
+//  Created by Dhau Embun Azzahra on 12/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Exercise_for_ElderlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
