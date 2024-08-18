2bf61ee1472ff390d7c861a36405b452369b7efc
embunazzahra <dhauembunazzahra@gmail.com>
2024-08-18 09:24:32 +0700

    add watch app

---
+++ ExerciseWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExerciseWatch Watch App
+//
+//  Created by Dhau Embun Azzahra on 18/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExerciseWatch Watch App/ExerciseWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseWatchApp.swift
+//  ExerciseWatch Watch App
+//
+//  Created by Dhau Embun Azzahra on 18/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
