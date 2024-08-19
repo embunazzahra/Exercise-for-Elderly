77c79b50a32ac2b211e92dabdb23699a0bdb0418
Hans Jonathan Zebua <hans.zebua@icloud.com>
2024-08-19 17:02:04 +0700

    bismillah

---
+++ ExerciseWatch Watch App/heartRateManager.swift
@@ -0,0 +1,39 @@
+//
+//  heartRateManager.swift
+//  ExerciseWatch Watch App
+//
+//  Created by Hans Zebua on 19/08/24.
+//
+
+import HealthKit
+
+class HeartRateManager {
+    
+    static let shared = HeartRateManager()
+    private let healthStore = HKHealthStore()
+    
+    // Function to start a heart rate query. It takes a completion handler that returns an optional array of HKSample.
+    func startHeartRateQuery(completion: @escaping ([HKSample]?) -> Void) {
+        
+        // To ensure the heart rate type is valid and If not valid, exit the function early.
+        guard let heartRateType = HKObjectType.quantityType(forIdentifier: .heartRate) else {
+            return
+        }
+        
+        // Create a predicate to query samples starting from the current date.
+        let predicate = HKQuery.predicateForSamples(withStart: Date(), end: nil, options: .strictStartDate)
+        
+        // Create an anchored object query to get heart rate samples.
+        let query = HKAnchoredObjectQuery(type: heartRateType, predicate: predicate, anchor: nil, limit: HKObjectQueryNoLimit) { (query, samples, deletedObjects, newAnchor, error) in
+            completion(samples)
+        }
+        
+        // Set the update handler to be called when new data is available.
+        query.updateHandler = { (query, samples, deletedObjects, newAnchor, error) in
+            completion(samples)
+        }
+        
+        // Execute the query on the healthStore.
+        healthStore.execute(query)
+    }
+}
---
+++ ExerciseWatch Watch App/heartRateModel.swift
@@ -0,0 +1,12 @@
+//
+//  heartRateModel.swift
+//  ExerciseWatch Watch App
+//
+//  Created by Hans Zebua on 19/08/24.
+//
+
+import Foundation
+
+struct HeartRateModel {
+    var heartRate: Double
+}
