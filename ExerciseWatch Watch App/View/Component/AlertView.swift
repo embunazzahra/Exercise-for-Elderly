3736425dd32fd8d2e46896be1bd4f418d23c5246
Hans Jonathan Zebua <hans.zebua@icloud.com>
2024-08-21 08:23:16 +0700

    update the vm and component

--- ExerciseWatch Watch App/View/Component/AlertView.swift
+++ ExerciseWatch Watch App/View/Component/AlertView.swift
@@ -7,28 +7,31 @@
 
 import SwiftUI
 
-struct AlertView: View {
-    var turnOffAlert: () -> Void
+struct AlertPopupView: View {
+    // Properties for customizing the popup
+    let message: String
+    let buttonText: String
+    let onButtonTapped: () -> Void
     
     var body: some View {
         VStack {
-            Text("BPM terlalu tinggi!")
+            Text(message)
                 .font(.headline)
                 .padding()
             
             Button(action: {
-                turnOffAlert()
+                onButtonTapped()
             }) {
-                Text("Tap to Turn Off")
+                Text(buttonText)
                     .foregroundColor(.red)
                     .padding()
                     .background(Color.white)
                     .cornerRadius(10)
             }
         }
+        .background(Color.black.opacity(0.6))
+        .cornerRadius(15)
+        .padding()
     }
 }
 
-#Preview {
-    AlertView(turnOffAlert: {})
-}
