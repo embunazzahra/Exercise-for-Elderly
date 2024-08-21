84a1e1fc7c6ba6b8ad3a1e683028cb708b7b3cae
embunazzahra <dhauembunazzahra@gmail.com>
2024-08-21 16:26:59 +0700

    delete concatenated code

--- Exercise-for-Elderly/View/Page/Join.swift
+++ Exercise-for-Elderly/View/Page/Join.swift
@@ -33,28 +33,18 @@ struct Join: View {
             Spacer()
             
             // Display concatenated codes
-            if !concatenatedCodes.isEmpty {
-                VStack {
-                    Text("Concatenated Codes:")
-                        .font(.headline)
-                    Text(concatenatedCodes)
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color(red: 0, green: 0.51, blue: 0.58))
-                }
-                .padding(.top, 20)
-            }
+//            if !concatenatedCodes.isEmpty {
+//                VStack {
+//                    Text("Concatenated Codes:")
+//                        .font(.headline)
+//                    Text(concatenatedCodes)
+//                        .font(.title)
+//                        .fontWeight(.bold)
+//                        .foregroundColor(Color(red: 0, green: 0.51, blue: 0.58))
+//                }
+//                .padding(.top, 20)
+//            }
             
-            HStack {
-                Text("1")
-                    .font(.system(size: 24))
-                    .fontWeight(.medium)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color(red: 0, green: 0.51, blue: 0.58))
-                Image(systemName: "person.fill")
-                    .foregroundColor(Color(red: 0, green: 0.51, blue: 0.58))
-            }
-            .frame(width: 343, alignment: .top)
             
             ButtoniOS(text: "Start", isPressed: isButtonPressed)
                 .disabled(!isButtonPressed)
